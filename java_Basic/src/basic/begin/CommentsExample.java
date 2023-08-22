3b79d078173703fc0cec9232df8e2642ad9e30f2
samkyung <rnflrntk@naver.com>
2023-08-22 17:14:54 +0900

    20230822 자바 기초 (표준 출력 방식까지)

---
+++ java_Basic/src/basic/begin/CommentsExample.java
@@ -0,0 +1,37 @@
+package basic.begin;
+
+/**
+ * 문서 생성용 주석입니다.
+ * 배포용 문서에 설명을 달아 놓을 때 사용합니다.
+ * 문서에 대한 설명 뿐만 아니라, 함수(메서드)의 사용방법 등을 설명하는 데에도 사용합니다.
+ * @author Sam
+ * @version 1.0
+ * @since 2023.08.22~
+ * @parm
+ * */
+
+//이 파일은 주석을 연습하는 파일이다.
+
+public class CommentsExample {
+
+	public static void main(String[] args) {
+		
+		/*
+		 # main 함수(메서드)는 프로그램 실행 시 자동으로 동작하는 함수이며,
+		  반드시 필요한 함수입니다.
+		  
+		  #자바 코드를 해석하며 실행시키는 자바 가상 머신 (JVM)은
+		   프로그램 실행 시 자동으로 main 함수를 불러서 내부의 코드를
+		   실행시킵니다.
+		 */
+		
+		//sysout + Ctrl + space: 빠른 출력 함수를 생성.
+		//Ctrl + f11 :  실행 단축키
+		//원하는 위치에 커서를 배치 후 Ctrl + / : 주석 단축키
+		System.out.println("안녕하세요~");
+		System.out.println("반갑습니다~");
+		
+		
+	}
+
+}
