763c1786ead0a37921ec1f634d6e29c05b9723eb
samkyung <rnflrntk@naver.com>
2023-09-01 17:42:30 +0900

    "20230901 수업"

---
+++ java_Basic/src/oop/final_/method/Child.java
@@ -0,0 +1,17 @@
+package oop.final_.method;
+
+public class Child extends Parent {
+
+	@Override
+	void method2() {
+		// TODO Auto-generated method stub
+		super.method2();
+	}
+	
+	
+//	void method3() {
+//		System.out.println("오버라이딩 해야지~");
+//	} (x) -> final 메서는 오버라이딩을 막습니다. (물려주는 대로 써라~)
+	
+	
+}
